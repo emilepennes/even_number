9c921ea199bac04f28426b6a094fd6fa6586f863
emile <51802782+emilepennes@users.noreply.github.com>
2019-06-22 12:24:47 +0200

    Update even_number.py

--- even_number.py
+++ even_number.py
@@ -2,10 +2,11 @@
 # The proposal of the exercice is to define whether the number is even or odd
 
 x = "x"
+print("The proposal of the exercice is to define whether the integer x is even or uneven.")
 
 while True :
     try :
-        x = int(input("Soit "+ x +" = "))
+        x = int(input("Let's have "+ x +" = "))
         break
     except ValueError :
         print(x, "must be an integer ;) Try again...")
@@ -14,4 +15,4 @@ if x % 2 == 0 :
     print(str(x) + " is an even number")
 
 else :
-    print(str(x) + " is an odd number")
+    print(str(x) + " is an uneven number")
